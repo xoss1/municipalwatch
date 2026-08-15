c8925f01f282e45fb7e193307157ffc090be05d0
xoss1 <55229811+xoss1@users.noreply.github.com>
2026-08-15 12:55:51 +0200

    Update __init__.py

--- src/municipalwatch/scrapers/__init__.py
+++ src/municipalwatch/scrapers/__init__.py
@@ -4,6 +4,7 @@ from .type_0 import extract_type_0
 # Mapeo extensible: cuando crees la plataforma tipo 1, solo agregas "1: extract_type_1"
 SCRAPERS = {
     0: extract_type_0
+    1: extract_type_1
 }
 
 def obtener_extractor(tipo_plataforma):
