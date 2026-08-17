12197dadfd6fb7baf0386b0dbaa1c3da686fe4c2
xoss1 <55229811+xoss1@users.noreply.github.com>
2026-08-17 09:33:22 +0200

    Update type_3.py

--- src/municipalwatch/scrapers/type_3.py
+++ src/municipalwatch/scrapers/type_3.py
@@ -11,11 +11,26 @@ def extract_type_3(session, item):
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36"
     }
 
+    payload = {
+        "aaxmlrequest": "true",
+        "eventScreenId": "TABLON",
+        "eventComponent": "",
+        "eventObject": "LISTATABLON",
+        "eventAction": "LISTATABLON",
+        "eventArguments": "KEY=all",
+        "PAGE_CODE": "TABLON",
+        "APP_CODE": "STA",
+        "PAGE_COMPLETE": "",
+        "ROOTID": "31",
+        "HFC": "HEADER_OVC#FOOTER_OVC",
+        "SESSION_REQUIRED": "false"
+    }
+
     try:
         url = item["url"]
         st.write(f"🔍 **[DEBUG] Escaneando {item['nombre']}:** `{url}`")
         
-        response = session.get(url, headers=headers, timeout=15, allow_redirects=True)
+        response = session.post(url, data=payload, headers=headers, timeout=15, allow_redirects=True)
         st.write(f"👉 **[DEBUG] Código HTTP respuesta:** `{response.status_code}`")
 
         if response.status_code != 200:
@@ -25,7 +40,31 @@ def extract_type_3(session, item):
         html_text = response.text
         st.write(f"📄 **[DEBUG] Longitud del HTML recibido:** `{len(html_text)}` caracteres")
 
-        ids_encontrados = re.findall(r'anuncio\.aspx\?id=(\d+)', html_text)
+        ids_encontrados = []
+
+        pattern = re.compile(
+            r'(?:\[\s*\{|,\s*\{)\s*'                      # Coincide con [{ o ,{ (inicio de elemento en el array)
+            r'"dboid"\s*:\s*"(?P<dboid>\d+)".*?'          # Captura solo el DBOID principal del objeto
+            r'"pubDateIni"\s*:\s*\{\s*'
+            r'"year"\s*:\s*(?P<year>\d+)\s*,\s*'
+            r'"month"\s*:\s*(?P<month>\d+)\s*,\s*'
+            r'"day"\s*:\s*(?P<day>\d+)\s*,\s*'
+            r'"timezone"\s*:\s*\d+\s*,\s*'
+            r'"hour"\s*:\s*(?P<hour>\d+)\s*,\s*'
+            r'"minute"\s*:\s*(?P<minute>\d+)\s*,\s*'
+            r'"second"\s*:\s*(?P<second>\d+)',
+            re.DOTALL
+        )
+
+        for match in pattern.finditer(texto_dataset):
+            d = match.groupdict()
+    
+            fecha_str = f"{int(d['year']):04d}{int(d['month']):02d}{int(d['day']):02d}{int(d['hour']):02d}{int(d['minute']):02d}{int(d['second']):02d}"
+            bloque_unico = d['dboid'][8:-5]
+    
+            id_sintetico = int(f"{fecha_str}{bloque_unico}")
+            ids_sinteticos.append(id_sintetico)
+            
         st.write(f"📄 **[DEBUG] id encontrados:** '{ids_encontrados}'")
         if ids_encontrados:
             return max([int(i) for i in ids_encontrados])
