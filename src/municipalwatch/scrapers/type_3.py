6a88c65d79f9ca2c2539438f623eb0254956b00d
xoss1 <55229811+xoss1@users.noreply.github.com>
2026-08-18 12:06:02 +0200

    Update type_3.py

--- src/municipalwatch/scrapers/type_3.py
+++ src/municipalwatch/scrapers/type_3.py
@@ -83,15 +83,16 @@ def extract_type_3(session, item):
     }
         try:
             session = requests.Session()
-            session_aa = session.get(url, impersonate="chrome", proxies=proxies)
+            session.verify = False
+            session_aa = session.get(url, impersonate="chrome", proxies=proxies, verify=False)
             time.sleep(1)
             st.write(f"🔍 **[DEBUG] Estatus de sesion GET {session_aa.status_code}:** `{url}`")
             st.write(f"🔍 **[DEBUG] Escaneando {item['nombre']}:** `{url}`")
 
             if esMazarron:
-                response = requests.get(item["referer"], proxies=proxies)
+                response = requests.get(item["referer"], proxies=proxies, verify=False)
             else:
-                response = requests.post(url, proxies=proxies, data=payload)
+                response = requests.post(url, proxies=proxies, data=payload, verify=False)
             st.write(f"👉 **[DEBUG] Código HTTP respuesta:** `{response.status_code}`")
     
             if response.status_code != 200:
