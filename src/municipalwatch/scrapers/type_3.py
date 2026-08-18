695294a75b24f97b994d05943aeafa15131ef1c3
xoss1 <55229811+xoss1@users.noreply.github.com>
2026-08-18 11:42:20 +0200

    Update type_3.py

--- src/municipalwatch/scrapers/type_3.py
+++ src/municipalwatch/scrapers/type_3.py
@@ -91,7 +91,7 @@ def extract_type_3(session, item):
     
             ids_encontrados = []
     
-            PATTERN = r'\{\s*"dboid"\s*:\s*"(?P<dboid>\d+)".*?"pubDateIni"\s*:\s*\{\s*(?:[^{}]*?"year"\s*:\s*(?P<year>\d+))(?:[^{}]*?"month"\s*:\s*(?P<month>\d+))(?:[^{}]*?"day"\s*:\s*(?P<day>\d+))(?:[^{}]*?"hour"\s*:\s*(?P<hour>\d+))(?:[^{}]*?"minute"\s*:\s*(?P<minute>\d+))(?:[^{}]*?"second"\s*:\s*(?P<second>\d+))'
+            pattern = r'\{\s*"dboid"\s*:\s*"(?P<dboid>\d+)".*?"pubDateIni"\s*:\s*\{\s*(?:[^{}]*?"year"\s*:\s*(?P<year>\d+))(?:[^{}]*?"month"\s*:\s*(?P<month>\d+))(?:[^{}]*?"day"\s*:\s*(?P<day>\d+))(?:[^{}]*?"hour"\s*:\s*(?P<hour>\d+))(?:[^{}]*?"minute"\s*:\s*(?P<minute>\d+))(?:[^{}]*?"second"\s*:\s*(?P<second>\d+))'
     
             for match in pattern.finditer(html_text):
                 d = match.groupdict()
