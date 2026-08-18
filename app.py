1369d3ce4861f7eb76e79de8a4b35a3e57b9d002
xoss1 <55229811+xoss1@users.noreply.github.com>
2026-08-18 10:33:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ if st.button("🚀 Iniciar Escaneo de Edictos", type="primary"):
         for nov in novedades:
             with st.expander(f"📍 {nov['seccion']} (Nuevo ID: {nov['id_nuevo']})"):
                 st.write(f"**ID Anterior:** {nov['id_anterior']} ➔ **ID Nuevo:** {nov['id_nuevo']}")
-                if nov['tipo']==0:
+                if nov['tipo'] in (0,3):
                     st.markdown(f"[🔗 Ver tablón de edictos]({nov['referer']})")
                 else:
                     st.markdown(f"[🔗 Ver tablón de edictos]({nov['url']})")
