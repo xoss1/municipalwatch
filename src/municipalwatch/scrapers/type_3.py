005cc710ce218a22b87a7f9ad90771c993b5eec7
xoss1 <55229811+xoss1@users.noreply.github.com>
2026-08-18 10:05:35 +0200

    Update type_3.py

--- src/municipalwatch/scrapers/type_3.py
+++ src/municipalwatch/scrapers/type_3.py
@@ -1,8 +1,9 @@
-import re
+from curl_cffi import requests
 import streamlit as st
 
 def extract_type_3(session, item):
     """Extractor para plataformas Tipo 3 ("Carpeta Ciudadana") con depuración en tiempo real."""
+    url = item["url"]
     headers = {
         "Accept": "text/xml",
         "Accept-Encoding": "gzip, deflate, br, zstd",
@@ -34,52 +35,71 @@ def extract_type_3(session, item):
         "SESSION_REQUIRED": "false"
     }
 
-    try:
-        session.get(item["referer"], timeout=10)
-        time.sleep(1)
-        url = item["url"]
-        st.write(f"🔍 **[DEBUG] Escaneando {item['nombre']}:** `{url}`")
+    lista_proxies = [
+        "http://hfzpvcaa:s4s7wxrw1fv1@64.137.96.74:6641/",
+        "http://hfzpvcaa:s4s7wxrw1fv1@31.59.20.176:6754/",
+        "http://hfzpvcaa:s4s7wxrw1fv1@31.56.127.193:7684/",
+        "http://hfzpvcaa:s4s7wxrw1fv1@45.38.107.97:6014/",
+        "http://hfzpvcaa:s4s7wxrw1fv1@198.105.121.200:6462/",
+        "http://hfzpvcaa:s4s7wxrw1fv1@198.23.243.226:6361/",
+        "http://hfzpvcaa:s4s7wxrw1fv1@38.154.185.97:6370/",
+        "http://hfzpvcaa:s4s7wxrw1fv1@84.247.60.125:6095/",
+        "http://hfzpvcaa:s4s7wxrw1fv1@142.111.67.146:5611/",
+        "http://hfzpvcaa:s4s7wxrw1fv1@191.96.254.138:6185/"
+    ]
+
+    for proxy in lista_proxies:
         
-        response = session.post(url, data=payload, headers=headers, timeout=15, allow_redirects=True)
-        st.write(f"👉 **[DEBUG] Código HTTP respuesta:** `{response.status_code}`")
-
-        if response.status_code != 200:
-            st.error(f"❌ La web devolvió un estado no válido: {response.status_code}")
-            return None
-
-        html_text = response.text
-        st.write(f"📄 **[DEBUG] Longitud del HTML recibido:** `{len(html_text)}` caracteres")
-
-        ids_encontrados = []
-
-        pattern = re.compile(
-            r'(?:\[\s*\{|,\s*\{)\s*'                      # Coincide con [{ o ,{ (inicio de elemento en el array)
-            r'"dboid"\s*:\s*"(?P<dboid>\d+)".*?'          # Captura solo el DBOID principal del objeto
-            r'"pubDateIni"\s*:\s*\{\s*'
-            r'"year"\s*:\s*(?P<year>\d+)\s*,\s*'
-            r'"month"\s*:\s*(?P<month>\d+)\s*,\s*'
-            r'"day"\s*:\s*(?P<day>\d+)\s*,\s*'
-            r'"timezone"\s*:\s*\d+\s*,\s*'
-            r'"hour"\s*:\s*(?P<hour>\d+)\s*,\s*'
-            r'"minute"\s*:\s*(?P<minute>\d+)\s*,\s*'
-            r'"second"\s*:\s*(?P<second>\d+)',
-            re.DOTALL
-        )
-
-        for match in pattern.finditer(texto_dataset):
-            d = match.groupdict()
-    
-            fecha_str = f"{int(d['year']):04d}{int(d['month']):02d}{int(d['day']):02d}{int(d['hour']):02d}{int(d['minute']):02d}{int(d['second']):02d}"
-            bloque_unico = d['dboid'][8:-5]
-    
-            id_sintetico = int(f"{fecha_str}{bloque_unico}")
-            ids_sinteticos.append(id_sintetico)
+        proxies = {
+        "http": proxy, 
+        "https": proxy
+    }
+        try:
+            session = requests.Session()
+            session_aa = session.get(url, impersonate="chrome", proxies=proxies)
+            time.sleep(1)
+            st.write(f"🔍 **[DEBUG] Escaneando {item['nombre']}:** `{url}`")
             
-        st.write(f"📄 **[DEBUG] id encontrados:** '{ids_encontrados}'")
-        if ids_encontrados:
-            return max([int(i) for i in ids_encontrados])
-    except Exception as e:
-        st.error(f"💥 **[DEBUG] Excepción capturada en {item['nombre']}:** `{e}`")
-        pass
+            response = session.post(url, proxies=proxies, data=payload, impersonate="chrome")
+            st.write(f"👉 **[DEBUG] Código HTTP respuesta:** `{response.status_code}`")
+    
+            if response.status_code != 200:
+                st.error(f"❌ La web devolvió un estado no válido: {response.status_code}")
+                return None
+    
+            html_text = response.text
+            st.write(f"📄 **[DEBUG] Longitud del HTML recibido:** `{len(html_text)}` caracteres")
+    
+            ids_encontrados = []
+    
+            pattern = re.compile(
+                r'(?:\[\s*\{|,\s*\{)\s*'                      # Coincide con [{ o ,{ (inicio de elemento en el array)
+                r'"dboid"\s*:\s*"(?P<dboid>\d+)".*?'          # Captura solo el DBOID principal del objeto
+                r'"pubDateIni"\s*:\s*\{\s*'
+                r'"year"\s*:\s*(?P<year>\d+)\s*,\s*'
+                r'"month"\s*:\s*(?P<month>\d+)\s*,\s*'
+                r'"day"\s*:\s*(?P<day>\d+)\s*,\s*'
+                r'"timezone"\s*:\s*\d+\s*,\s*'
+                r'"hour"\s*:\s*(?P<hour>\d+)\s*,\s*'
+                r'"minute"\s*:\s*(?P<minute>\d+)\s*,\s*'
+                r'"second"\s*:\s*(?P<second>\d+)',
+                re.DOTALL
+            )
+    
+            for match in pattern.finditer(texto_dataset):
+                d = match.groupdict()
+        
+                fecha_str = f"{int(d['year']):04d}{int(d['month']):02d}{int(d['day']):02d}{int(d['hour']):02d}{int(d['minute']):02d}{int(d['second']):02d}"
+                bloque_unico = d['dboid'][8:-5]
+        
+                id_sintetico = int(f"{fecha_str}{bloque_unico}")
+                ids_sinteticos.append(id_sintetico)
+                
+            st.write(f"📄 **[DEBUG] id encontrados:** '{ids_encontrados}'")
+            if ids_encontrados:
+                return max([int(i) for i in ids_encontrados])
+        except Exception as e:
+            st.error(f"💥 **[DEBUG] Excepción capturada en {item['nombre']}:** `{e}`")
+            pass
 
     return None
