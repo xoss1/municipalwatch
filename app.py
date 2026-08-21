12cda6821fe90b29bbfd3a9d1761ded8b24bd17e
xoss1 <55229811+xoss1@users.noreply.github.com>
2026-08-21 12:33:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -87,11 +87,11 @@ if st.button("🚀 Iniciar Escaneo de Edictos", type="primary"):
                         id_anterior = historial.get(nombre, 0)
                         if id_actual > id_anterior:
                             novedades.append({
-                                "titulo" = resultados["titulo"]
-                                "id" = resultados["id"]
-                                "fecha_pub" = resultados["fecha_publicacion"]
-                                "fecha_ret" = resultados["fecha_retirada"]
-                                "cod_ext" = resultados["codigo_expediente"]
+                                "titulo": resultados["titulo"],
+                                "id": resultados["id"]
+                                "fecha_pub": resultados["fecha_publicacion"],
+                                "fecha_ret": resultados["fecha_retirada"],
+                                "cod_ext": resultados["codigo_expediente"]
                             })
                             historial[nombre] = id_actual
                 
