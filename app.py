72da3dba9ad9563d7c86e686d0aced07aa138b97
xoss1 <55229811+xoss1@users.noreply.github.com>
2026-08-22 16:56:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -269,7 +269,9 @@ if novedades:
                                 st.text(f"Publicación: {fecha_pub or '-'} | Retirada: {item.get('fecha_ret', '-')}")
                             st.divider()
                 else:
-                    st.write("No hay detalles desglosados disponibles para esta sección.")
+                    #st.write("No hay detalles desglosados disponibles para esta sección.")
+                    pass
+                    
 elif "novedades" in st.session_state:
     st.info("Cero novedades en todas las páginas rastreadas.")
 # Vista rápida del historial guardado
