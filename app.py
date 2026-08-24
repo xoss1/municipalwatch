06eebad9bf5f3e9ec7dfb93a1d8fedf1485a85bc
xoss1 <55229811+xoss1@users.noreply.github.com>
2026-08-24 21:55:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -213,7 +213,7 @@ if not devMode:
         lanzar = True
 novedades = cargar_novedades()
 # Botón de escaneo
-if lanzar and not scan_reciente():
+if lanzar:
     progress_bar = st.progress(0)
 
     def actualizar_progreso(valor):
